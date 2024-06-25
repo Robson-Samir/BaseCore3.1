c415b65f5d706ef878a11b77b543b5b5aefe77c3
= <=>
2024-06-25 16:40:18 -0300

    Chore: Ajuste de template #2

---
+++ Base.Application.Api/Controllers/PedidoController.cs
@@ -0,0 +1,35 @@
+﻿using Base.Application.Services.Interfaces;
+using Base.Domain.Entities.Domain;
+using Base.Infrastructure.CrossCutting.Utilities.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Base.Application.Api.Controllers
+{
+    [Route("api/pedido")]
+    [ApiController]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IPedidoAppService appAppService;
+
+        public PedidoController(IPedidoAppService pedidoAppService)
+        {
+            appAppService = pedidoAppService;
+        }
+
+        [Route("cadpedido")]
+        [HttpPost]
+        public ResultadoOperacao<bool> CadastroPedido(Pedido pedido)
+        {
+            var resull = appAppService.CadastroPedido(pedido);
+            return new ResultadoOperacao<bool>(resull);
+        }
+
+        [Route("caduser")]
+        [HttpGet]
+        public ResultadoPesquisa<bool>  CadastroUsuario(Usuario usuario)
+        {
+            var result = appAppService.CadastroUsuario(usuario);
+            return new ResultadoPesquisa<bool>(result);
+        }
+    }
+}
